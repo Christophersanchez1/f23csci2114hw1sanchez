d5d0db941d065741b2af99355e434634321725df
Christophersanchez1 <122641788+Christophersanchez1@users.noreply.github.com>
2023-08-23 15:08:52 -0500

    Add files via upload

---
+++ OneSanchez.java
@@ -0,0 +1,5 @@
+public class OneSanchez {
+    public static void main(String[] args) {
+        System.out.println("Why do fathers take an extra pair of socks when they go golfing?In case they get a hole in one");
+    }
+}
